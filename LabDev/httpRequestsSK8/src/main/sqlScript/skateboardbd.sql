488f36d64e27985266317d70c90e28449b01c997
Carlos Daniel <162034625+darloscaniel@users.noreply.github.com>
2025-03-10 18:20:11 -0300

    fix: fixing sql columns in boards table

--- LabDev/httpRequestsSK8/src/main/sqlScript/skateboardbd.sql
+++ LabDev/httpRequestsSK8/src/main/sqlScript/skateboardbd.sql
@@ -3,10 +3,13 @@ CREATE DATABASE skateboardbd;
 CREATE TABLE boards (
     id SERIAL PRIMARY KEY,
     brand VARCHAR(255) NOT NULL,
-    size VARCHAR(50) NOT NULL
+    size VARCHAR(50) NOT NULL,
+	imgUrl VARCHAR(255)
 );
 
-INSERT INTO boards (brand, size) VALUES
-('Element', '8.0'),
-('Santa Cruz', '8.25'),
-('Powell Peralta', '7.75');
+INSERT INTO boards (brand, size, imgUrl) VALUES
+('Element', '8.0', 'https://images.tcdn.com.br/img/img_prod/17375/shape_element_scipt_8_0_3077_1_20190223100308.jpg'),
+('Santa Cruz', '8.25', 'https://th.bing.com/th/id/R.a175e92d28fa4c246f487b6bb791b8ef?rik=1S1LhMf7x3T8%2bA&pid=ImgRaw&r=0'),
+('Powell Peralta', '7.75', 'https://th.bing.com/th/id/OIP.V6cG3k6ZzgaAHzKSsQf9eQHaHi?rs=1&pid=ImgDetMain');
+
+select * from boards;
